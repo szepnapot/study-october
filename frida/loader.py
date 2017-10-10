5c0485be0e34f3b38f80fecc6af0594240879167
Peter Lodri <silentpocok@gmail.com>
2017-10-10 10:08:22 +0200

    init

--- frida/loader.py
+++ frida/loader.py
@@ -8,7 +8,16 @@ print("PID: {}".format(pid))
 device.resume(pid)
 time.sleep(1) #Without it Java.perform silently fails
 session = device.attach(pid)
-script = session.create_script(open("s1.js").read())
+with open("s2.js") as f:
+    script = session.create_script(f.read())
+script.load()
+script.post(pid)
+
+def my_message_handler(message , payload): #define our handler
+    print message
+    print payload
+
+script.on("message", my_message_handler) #register our handler to be called
 script.load()
 
 #prevent the python script from terminating
---
+++ frida/loader_s3.py
@@ -0,0 +1,27 @@
+import time
+
+import frida
+
+
+def my_message_handler(message, payload):
+    print message
+    print payload
+
+
+device = frida.get_usb_device()
+pid = device.spawn(["com.example.a11x256.frida_test"])
+device.resume(pid)
+time.sleep(1)  # Without it Java.perform silently fails
+session = device.attach(pid)
+with open("s3.js") as f:
+    script = session.create_script(f.read())
+script.on("message", my_message_handler)
+script.load()
+
+command = ""
+while 1 == 1:
+    command = raw_input("Enter command:\n1: Exit\n2: Call secret function\nchoice:")
+    if command == "1":
+        break
+    elif command == "2":
+        script.exports.callSecretFunction()
